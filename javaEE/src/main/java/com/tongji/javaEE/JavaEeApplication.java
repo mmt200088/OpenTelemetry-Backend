02be5581a6487eafa6b72141ec5788e0efa6ff63
needle1989 <edwardskirk@163.com>
2020-12-28 15:03:17 +0800

    metrics init

--- javaEE/src/main/java/com/tongji/javaEE/JavaEeApplication.java
+++ javaEE/src/main/java/com/tongji/javaEE/JavaEeApplication.java
@@ -1,21 +1,48 @@
 package com.tongji.javaEE;
 
+import com.tongji.javaEE.Service.Metrics.ValueObserver;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.IOException;
+
+/**
+ * @author Raven
+ */
 @SpringBootApplication
 @RestController
 public class JavaEeApplication {
 
+	public JavaEeApplication() throws IOException {
+	}
+
 	public static void main(String[] args) {
 		SpringApplication.run(JavaEeApplication.class, args);
 	}
+
+	/**
+	 * AIOps
+	 */
+	ValueObserver valueObserver = new ValueObserver(19090);
+	/**
+	 * AIOps
+	 */
+
 	@GetMapping("/hello")
 	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+
+		/**
+		 * AIOps
+		 */
+		long value = name.length();
+		valueObserver.incomingMessageCount = value;
+		/**
+		 * AIOps
+		 */
+
 		return String.format("Hello %s!", name);
 	}
 
---
+++ javaEE/src/main/java/com/tongji/javaEE/Service/Metrics/ValueObserver.java
@@ -0,0 +1,44 @@
+package com.tongji.javaEE.Service.Metrics;
+
+import io.opentelemetry.api.common.Labels;
+import io.opentelemetry.api.metrics.GlobalMetricsProvider;
+import io.opentelemetry.api.metrics.LongValueObserver;
+import io.opentelemetry.api.metrics.Meter;
+import io.opentelemetry.api.metrics.MeterProvider;
+import io.opentelemetry.exporter.prometheus.PrometheusCollector;
+import io.opentelemetry.sdk.metrics.SdkMeterProvider;
+import io.prometheus.client.exporter.HTTPServer;
+
+import java.io.IOException;
+/**
+ * Example of using the {@link PrometheusCollector} to convert OTel metrics to Prometheus format and
+ * expose these to a Prometheus instance via a {@link HTTPServer} exporter.
+ *
+ * <p>A {@link LongValueObserver} is used to periodically measure how many incoming messages are
+ * awaiting processing. The {@link LongValueObserver} Updater gets executed every collection
+ * interval.
+ * @author Raven
+ */
+public class ValueObserver {
+
+    private final MeterProvider meterSdkProvider = GlobalMetricsProvider.get();
+    private final Meter meter = meterSdkProvider.get("PrometheusExample", "0.13.1");
+    private final HTTPServer server;
+    public long incomingMessageCount;
+
+    public ValueObserver(int port) throws IOException {
+        LongValueObserver observer =
+                meter
+                        .longValueObserverBuilder("incoming.messages")
+                        .setDescription("No of incoming messages awaiting processing")
+                        .setUnit("message")
+                        .setUpdater(result -> result.observe(incomingMessageCount, Labels.empty()))
+                        .build();
+
+        PrometheusCollector.builder()
+                .setMetricProducer(((SdkMeterProvider) meterSdkProvider).getMetricProducer())
+                .buildAndRegister();
+
+        server = new HTTPServer(port);
+    }
+}
